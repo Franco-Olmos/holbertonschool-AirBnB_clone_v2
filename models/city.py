cdb88f5a8719cd64e86c3e29897997cdabc78fda
Franco-Olmos <5749@holbertonstudents.com>
2023-03-23 16:28:50 -0700

    fix try

--- models/city.py
+++ models/city.py
@@ -8,6 +8,10 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='user')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+    "places = relationship('Place', cascade='all, delete', backref='user')"
+    else:
+        state_id = ''
+        name = ''
