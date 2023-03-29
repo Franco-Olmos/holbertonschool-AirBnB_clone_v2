daa7da1420fbeb586971b258bf334becfe1444da
NMenendez24 <nahuelmt24@gmail.com>
2023-03-29 15:23:56 -0300

    Task 4 typo

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,7 +29,7 @@ def python_print(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/numeber/<n>', strict_slashes=False)
+@app.route('/number/<n>', strict_slashes=False)
 def number_print(n):
     """Returns a statement only if n is a integer"""
     if isinstance(n, int):
