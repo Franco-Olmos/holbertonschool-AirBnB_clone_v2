42b82e7c60300c224c8fa685cd673037c4034eb6
NMenendez24 <nahuelmt24@gmail.com>
2023-03-29 13:23:18 -0300

    Change import statement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Runs a simple flask app"""
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
